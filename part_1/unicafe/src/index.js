be12cf1d77de55514e44a59c09d56c536f075bdc
Teyeb H <hussenab@gmail.com>
2020-07-09 01:13:43 +0300

    exercise 1.6-1.10 done

--- part_1/unicafe/src/index.js
+++ part_1/unicafe/src/index.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 const App = () => {
+  
+  const [good,setGood] = useState(0)
+  const [neutral,setNeutral] = useState(0)
+  const [bad,setBad] = useState(0)
+  const [all , setAll] = useState(0)
+ 
 
+  
+  const average = (all) => (all/3).toFixed(1)
+  const percentage = (feedback, all) => {
+    if (all > 0){
+      return (feedback/ all * 100).toFixed(2) * 100/ 100
+    }
+  }  
+  
+  
   return (
     <>
-    <h1>Hello Unicafe</h1>
-    </>
+      <h1>Give feedback</h1>
+      <Button handleClick={() => { 
+        setGood(good + 1)
+        setAll(all + 1)}} text='Good' />
+      <Button handleClick={() => {
+        setNeutral(neutral + 1)
+        setAll(all + 1)
+        }} text='Neutral' />
+      <Button handleClick={() => {
+        setBad(bad + 1)
+        setAll(all + 1)
+        }} text='Bad' />   
+      
+      <h4>Statistics </h4>
+      <Statistics good={good}  neutral={neutral} bad={bad} average={average(all)} percentage={percentage(good, all)} sign={(average(all) > 0)? '%': ''}/>
+      </>
   )
 }
+const Button = ({handleClick, text}) => <button onClick={handleClick}> {text} </button>
+
+const Statistics = (props) => {
+  if(props.average == 0){
+    return (
+      <>
+        No feedback to display yet. 
+      </>
+    )
+
+  }
+  return (
+  <>
+      <Statistic name={'Good'} value={props.good} /> 
+      <Statistic name={'Neutral'} value={props.neutral} /> 
+      <Statistic name={'Bad'} value={props.bad} /> 
+      <Statistic name={'Average'} value={props.average} /> 
+      <Statistic name={'Positive'} value={props.percentage} sign={props.sign}/> 
+  </>
+  ) 
+}
+
+const Statistic = ({ name, value, sign}) => <p> {name} {value} {sign}</p>
 
 ReactDOM.render(
   <React.StrictMode>
